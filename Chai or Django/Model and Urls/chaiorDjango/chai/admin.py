5ebfcbcc4d0e0f27a518211c8ae266aa7c757708
Prince Sharma <krrishsharma698@gmail.com>
2026-05-31 16:35:05 +0530

    model relationsship

--- Chai or Django/Model and Urls/chaiorDjango/chai/admin.py
+++ Chai or Django/Model and Urls/chaiorDjango/chai/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
-from .models import ChaiVarity
+from .models import ChaiVarity, ChaiReview, ChaiCertificate, Store 
 # Register your models here.
-admin.site.register(ChaiVarity)
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    # model = ChaiVarity
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificteAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+admin.site.register(ChaiVarity, ChaiVarityAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificteAdmin)
+# admin.site.register()
